715920b6c5ceedafc75f37beaf2525256720a893
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-05-30 11:21:07 +0200

    Added 2nd version <- thanks to hcoetzee

---
+++ norm2.sh
@@ -0,0 +1,4 @@
+echo Hello user, please allow me to access your Libft so I can consume and copy all the files to my private... I mean Norme your files...
+
+find . "*.c" -exec norminette {} \;
+find . "*.h" -exec norminette {} \;
