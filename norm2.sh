24015742f65e4e3ceb168c31018aaa772f11f73c
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-05-30 11:22:55 +0200

    Removed

--- norm2.sh
+++
@@ -1,4 +0,0 @@
-echo Hello user, please allow me to access your Libft so I can consume and copy all the files to my private... I mean Norme your files...
-
-find . "*.c" -exec norminette {} \;
-find . "*.h" -exec norminette {} \;
