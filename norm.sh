842f2c65b8ba384b72fcd0bf33e1af5944376962
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-05-30 08:10:24 +0200

    Update

--- norm.sh
+++ norm.sh
@@ -1,12 +1,15 @@
 echo Hello user, please allow me to access your Libft so I can consume and copy all the files to my private... I mean Norme your files...
 
-for file in ./*c
+dir=./
+head=./
+
+for file in $src*c
 do
 	echo copying and normining $file
 	norminette $file
 	wait
 done
-for file in ./*.h
+for file in $head*.h
 do
 	echo copying and norming $file
 	norminette $file
