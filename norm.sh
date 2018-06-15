2f5bcc60bbc10fa3e17ef4ebb60537b3bd266e4e
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-15 07:40:34 +0200

    auto commit

--- norm.sh
+++ norm.sh
@@ -5,71 +5,58 @@ head=./includes/
 
 SECONDS=0
 
-#for file in $src*c
-#do
-#	echo "copying and normining $file "
-#	norminette $file
-#	wait
-#done
-#for file in $head*.h
-#do
-#	echo "copying and norming $file "
-#	norminette $file
-#	wait
-#done
-
-norminette $src/ft_a*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_a*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_b*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_b*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_c*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_c*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_d*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_d*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_e*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_e*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_f*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_f*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_g*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_g*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_h*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_h*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_i*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_i*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_j*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_j*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_k*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_k*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_l*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_l*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_m*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_m*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_n*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_n*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_o*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_o*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_p*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_p*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_q*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_q*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_r*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_r*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_s*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_s*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_t*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_t*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_u*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_u*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_v*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_v*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_w*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_w*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_x*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_x*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_y*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_y*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $src/ft_z*.c | sed -e 's/^Warning: Not a valid file*//'
+norminette $src/ft_z*.c | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 wait
-norminette $head/*.h | sed -e 's/^Warning: Not a valid file*//'
+norminette $head/*.h | sed -e 's/^Warning: Not a valid file*/--No Matching Files--/'
 DUR=$SECONDS
 echo "TIMELAPSE::: $(($DUR/60)) mins and $(($DUR%60)) seconds"
